979cf2230bb9e6fc6cfeff72a60702907542d539
Clement CHAMEYRAT <velocitypulse@outlook.fr>
2017-01-09 16:38:08 +0100

    first test

--- includes/include.h
+++ includes/include.h
@@ -6,8 +6,21 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 12:42:29 by cchameyr          #+#    #+#             */
-/*   Updated: 2017/01/09 12:42:30 by cchameyr         ###   ########.fr       */
+/*   Updated: 2017/01/09 15:09:11 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef _INCLUDE_H
+# define _INCLUDE_H
 
+# include <unistd.h>
+# include <dirent.h>
+//# include <sys/stat.h>
+# include <stdlib.h>
+//# include <dirent.h>
+
+# include "../libft/libft.h"
+
+#include "debug.h"
+
+#endif
