42d2cbd6a3ca1cd1331ba4fc7ecd7f101ab58307
Clement CHAMEYRAT <velocitypulse@outlook.fr>
2017-01-11 00:17:55 +0100

    ajout dans le main

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/09 12:42:16 by cchameyr          #+#    #+#             */
-/*   Updated: 2017/01/10 15:49:04 by cchameyr         ###   ########.fr       */
+/*   Updated: 2017/01/11 00:17:43 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,21 +35,75 @@ int		main(int argc, char **argv)
 					ft_printf("\t%s\n", dir1->d_name);
 		}
 	}
-
 	return (0);
 }
-
 */
+
+static void		init_data(t_ls *ls)
+{
+	ls->flag_l = false;
+	ls->flag_rec = false;
+	ls->flag_rev = false;
+	ls->flag_t = false;
+	ls->paths = NULL;
+}
+
+static void		get_flags(t_ls *ls, char *str)
+{
+	int		i;
+
+	i = -1;
+	while (str[++i])
+	{
+		if (str[i] == 'l')
+			ls->flag_l = true;
+		else if (str[i] == 'R')
+			ls->flag_rec = true;
+		else if (str[i] == 'r')
+			ls->flag_rev = true;
+		else if (str[i] == 't')
+			ls->flag_t = true;
+		else
+		{
+			ft_printf("ft_ls: illegal option -- %c\n", str[i]);
+			ls_exit("usage: ft_ls [-Rlrt] [file ...]\n");
+		}
+	}
+}
+
+static void		add_path(t_path **paths, char *str)
+{
+	t_path		*p;
+
+	if (*paths == NULL)
+	{
+		*paths = ft_memalloc(sizeof(t_path));
+		(*paths)->path = str;
+		(*paths)->next = NULL;
+	}
+	else
+	{
+		p = *paths;
+		while (p->next)
+			p = p->next;
+		p->next = ft_memalloc(sizeof(t_path));
+		p = p->next;
+		p->path = str;
+		p->next = NULL;
+	}
+}
 
 static int		get_arg(t_ls *ls, int ac, char **av)
 {
 	int		i;
 
 	i = 1;
+	while (i < ac && av[i][0] == '-' && av[i][1])
+		get_flags(ls, &av[i++][1]);
 	while (i < ac)
-	{
-		
-	}
+		add_path(&ls->paths, av[i++]);
+	if (ls->paths == NULL)
+		add_path(&ls->paths, ".");
 	return (_SUCCESS_);
 }
 
@@ -57,6 +111,7 @@ int		main(int argc, char **argv)
 {
 	t_ls	data;
 
-	get_arg(&data);
+	init_data(&data);
+	get_arg(&data, argc, argv);
 	return (0);
 }
